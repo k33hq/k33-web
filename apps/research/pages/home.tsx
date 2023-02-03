3c3750f8f8082652c60c1691067eb371a88be74b
Joel DSouza <joeldsouzax@gmail.com>
2023-02-03 19:46:48 +0100

    feat(research): added conditional check on home cover articles

--- apps/research/pages/home.tsx
+++ apps/research/pages/home.tsx
@@ -21,15 +21,15 @@ const Home: NextPage<HomeProps> = ({
     <>
       <section className="w-full bg-bg-light-secondary">
         <div className="md:container py-16 flex flex-col gap-12">
-          <CoverArticle {...mainArticle} />
+          {mainArticle ? <CoverArticle {...mainArticle} /> : null}
           <div
             id="secondary-articles-section"
             className="flex flex-row flex-wrap gap-12 items-center justify-between"
           >
-            <SubArticle {...subArticle1} />
-            <SubArticle {...subArticle2} />
-            <SubArticle {...subArticle3} />
-            <SubArticle {...subArticle4} />
+            {subArticle1 ? <SubArticle {...subArticle1} /> : null}
+            {subArticle2 ? <SubArticle {...subArticle2} /> : null}
+            {subArticle3 ? <SubArticle {...subArticle3} /> : null}
+            {subArticle4 ? <SubArticle {...subArticle4} /> : null}
           </div>
         </div>
       </section>
