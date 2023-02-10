69697e3a972cfe456ea2832a6d22e6ba21f3a8bd
Joel DSouza <joeldsouzax@gmail.com>
2023-02-10 14:32:58 +0100

    feat(research): added auth to the research home page

--- apps/research/src/layouts/PrivateLayout.tsx
+++ apps/research/src/layouts/PrivateLayout.tsx
@@ -1,7 +1,7 @@
 import config from '@/firebase/config';
 import * as React from 'react';
-import { init } from 'core';
-import { Modal, BrandButton } from 'ui';
+import { AppStates, getAppState, googleLogin, init, isLoggedIn } from 'core';
+import { Modal, BrandButton, Loading } from 'ui';
 import google from '../assets/google.svg';
 import Image from 'next/image';
 
@@ -10,28 +10,63 @@ interface PrivateLayoutProps {
 }
 
 const PrivateLayout: React.FC<PrivateLayoutProps> = ({ children }) => {
-  init(config);
+  const [state, setState] = React.useState<AppStates>('LOADING');
+
+  React.useEffect(() => {
+    init(config);
+  }, []);
+
+  React.useEffect(() => {
+    if (isLoggedIn()) {
+      setState('REGISTRED');
+    } else {
+      setState('SIGNED_OUT');
+    }
+  }, []);
+
+  const handleGoogleLogin = () => {
+    googleLogin(
+      (user) => {
+        setState('REGISTRED');
+      },
+      (err) => console.log(err)
+    );
+  };
+
   return (
     <>
       {children}
-      <Modal>
-        <div className="flex flex-col gap-6 items-center text-center">
-          <p className="text-heading5 text-label-light-secondary">Welcome to</p>
-          <p className="text-heading3 text-label-light-primary">K33 Research</p>
-          <p className="text-heading8 text-label-light-secondary">
-            Sing in for full access to our research archive of articles, reports
-            and analysis, and become an expert!
-          </p>
-          <div id="login-mechanism" className="flex flex-col pt-10 w-full">
-            <BrandButton
-              logo={
-                <Image src={google} width={22} height={22} alt="google logo" />
-              }
-              label="Sign in with Google"
-            ></BrandButton>
+      {state === 'SIGNED_OUT' ? (
+        <Modal>
+          <div className="flex flex-col gap-6 items-center text-center">
+            <p className="text-heading5 text-label-light-secondary">
+              Welcome to
+            </p>
+            <p className="text-heading3 text-label-light-primary">
+              K33 Research
+            </p>
+            <p className="text-heading8 text-label-light-secondary">
+              Sing in for full access to our research archive of articles,
+              reports and analysis, and become an expert!
+            </p>
+            <div id="login-mechanism" className="flex flex-col pt-10 w-full">
+              <BrandButton
+                logo={
+                  <Image
+                    src={google}
+                    width={22}
+                    height={22}
+                    alt="google logo"
+                  />
+                }
+                label="Sign in with Google"
+                onClick={handleGoogleLogin}
+              />
+            </div>
           </div>
-        </div>
-      </Modal>
+        </Modal>
+      ) : null}
+      {state === 'LOADING' ? <Loading /> : null}
     </>
   );
 };
