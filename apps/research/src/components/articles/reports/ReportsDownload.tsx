32b0bd0ca5ad265b869a0636af3f15e3b2a30f15
Joel DSouza <joeldsouzax@gmail.com>
2023-02-11 02:10:34 +0100

    feat(research): added author section and divided the article page in the proper way

--- apps/research/src/components/articles/reports/ReportsDownload.tsx
+++ apps/research/src/components/articles/reports/ReportsDownload.tsx
@@ -1,5 +1,7 @@
+import { useK33App } from '@/hooks';
 import * as React from 'react';
 import { BasicButton } from 'ui';
+import { GrDocumentLocked } from 'react-icons/gr';
 
 interface ReportsDownloadProps {
   url: string;
@@ -35,9 +37,20 @@ const ReportsDownload: React.FC<ReportsDownloadProps> = ({ url, title }) => {
       })
       .catch((e) => console.error(e));
   }
+
+  const [state] = useK33App();
+
+  //TODO: people can still disable this and download the report from the console easy fix but okay.
   return (
-    <BasicButton fullWidth onClick={() => downloadResource(url)}>
-      Download Report
+    <BasicButton
+      fullWidth
+      onClick={() => downloadResource(url)}
+      disabled={['LOADING', 'SIGNED_OUT'].includes(state)}
+    >
+      <div className="flex flex-row items-center justify-center gap-2">
+        <GrDocumentLocked className="inherit" />
+        Download Report
+      </div>
     </BasicButton>
   );
 };
