ed5a6037ccd45e97f911b4e9bbabc8515cbff5fd
Joel DSouza <joeldsouzax@gmail.com>
2023-02-11 02:45:16 +0100

    feat(research): added author in the article

--- apps/research/src/components/author/Profile.tsx
+++ apps/research/src/components/author/Profile.tsx
@@ -1,9 +1,37 @@
+import { Author } from '@/types';
+import Image from 'next/image';
 import * as React from 'react';
 
-interface ProfileProps {}
+interface ProfileProps extends Author {}
 
-const Profile: React.FC<ProfileProps> = () => {
-  return <div></div>;
+const Profile: React.FC<ProfileProps> = ({ name, title, profilePicture }) => {
+  return (
+    <div
+      id={`profile-${name.toLowerCase().replace(' ', '-')}`}
+      className="flex flex-row items-center gap-2"
+    >
+      <div
+        className="flex h-[48px] w-[48px]"
+        style={{
+          position: 'relative',
+        }}
+      >
+        <Image
+          src={profilePicture.url}
+          alt={profilePicture.description}
+          fill
+          style={{
+            objectFit: 'cover',
+            borderRadius: '100px',
+          }}
+        />
+      </div>
+      <div className="flex flex-col">
+        <p className="text-body1 text-label-light-primary">{name}</p>
+        <p className="text-small text-label-light-secondary">{title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
--- apps/research/src/components/index.ts
+++ apps/research/src/components/index.ts
@@ -7,3 +7,4 @@ export * from './categories';
 export * from './research';
 export { default as ResearchHeader } from './ResearchHeader';
 export { default as ResearchFooter } from './ResearchFooter';
+export * from './author';
