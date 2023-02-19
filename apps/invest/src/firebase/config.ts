3a7397f81cfddd13a7f862e44329105bf245b5ed
Vihang Patil <vihang.patil@gmail.com>
2023-02-20 01:14:06 +0530

    fix: Corrected and reordered environment variables

--- apps/invest/src/firebase/config.ts
+++ apps/invest/src/firebase/config.ts
@@ -3,11 +3,12 @@ import type { FirebaseOptions } from 'firebase/app';
 // TODO: move to platform-js
 const config: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export default config;
