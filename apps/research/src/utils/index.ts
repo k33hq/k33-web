5424227041bbb6d8a6c8b60422037d7ae699580e
Joel DSouza <joeldsouzax@gmail.com>
2023-02-22 14:01:53 +0100

    fix(research): added sitemap generat

--- apps/research/src/utils/index.ts
+++ apps/research/src/utils/index.ts
@@ -1,3 +1,28 @@
+import { ArchivePageSlugs } from '@/types';
+// import fs from 'fs';
+
+// export const generateSitemap = async (posts: ArchivePageSlugs) => {
+//   const sitemap = `
+// <?xml version="1.0" encoding="UTF-8"?>
+// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+// ${[...posts]
+//   .map(
+//     (post) => `<url>
+//   <loc>${process.env.NEXT_PUBLIC_RESEARCH_URL}/${post.slug}</loc>
+//   <lastmod>${post.sys.publishedAt}</lastmod>
+// </url>`
+//   )
+//   .join('')}
+// </urlset>`.replace(',', '');
+
+//   // writes sitemap.xml to public directory
+//   const path = `${process.cwd()}/public/sitemap.xml`;
+
+//   fs.writeFileSync(path, sitemap, 'utf8');
+
+//   return sitemap;
+// };
+
 export const getUrl = (...slugs: Array<string>) => `/${slugs.join('/')}`;
 
 export const colors = {
@@ -14,6 +39,7 @@ export const colors = {
 
 export * from './get-stripejs';
 
+// TODO: put these in some lib
 export function forceDownload(blob: string, filename?: string | undefined) {
   var a = document.createElement('a');
   //@ts-ignore
