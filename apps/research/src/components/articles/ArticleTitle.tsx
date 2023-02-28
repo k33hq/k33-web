fa3235a31f4a616949cf8971db43393d37180f41
Joel D'Souza <joeldsouzax@gmail.com>
2023-02-28 11:01:36 +0100

    Bug fixes (#24)
    * Merge branch 'main' of github.com:33k33/k33-web
    
    * fix(kvq): fixed mobile css for kvq
    
    * feat(research): added archived articles
    
    * feat(research): updated the archive path instead of archives
    
    * feat(research): updated the workflows
    
    * feat(research): updated the env variables
    
    * fix(research): updated the limit for the archive articles
    
    * fix(research): removed linkToReport
    
    * fix()
    
    * fix(research):
    
    * fix(research): fixed missing fields in archived reports
    
    * fix(research): added stripe key back again
    
    * fix(research): fixed the stripe success url problem
    
    * fix(research): added the research path
    
    * fix(research): added sitemap generat
    
    * fix(research): only showing reports for now in research
    
    * feat(research): added clickable article elements
    
    * fix(research): added linking to article elements and made it look a bit better on screens
    
    * fix(research): added sub article description line clamping
    
    * feat(research): added subscription promotion box
    
    * fix(research): made the page look acceptable in mobile phones
    
    * fix(research): fixed the login modal for research
    
    * feat(research): added analyst bar
    
    * fix(research): fixed the talk to the analyst button
    
    * fix(research): added mailto link to the analyst box
    
    * fix(research)
    
    * fix(research): made the category page look good for mobile phones
    
    * fix(research): updated the published date for categories
    
    * feat(platform): added social twitter and email links to the footer
    
    * refactor(platform): rearranged the social links
    
    * fix(research): added social links to the secondary header
    
    * fix(research): updated the svg names
    
    * fix(research): updated the article sorting
    
    * fix(research): updated the date time and fixed the published Date problem
    
    * fix(research): updated article date for category product
    
    * fix(research): updated the headings for reports and articles
    
    * feat(research): added highlighted products
    
    * fix(research): updated the queries for articles and categories
    
    * fix(research): added opinion and analysis check
    
    * feat(research): added subscription feature
    
    * fix(research): fixed the opinion and analysis list
    
    * feat(research): stripe subscriber updated
    
    * feat(invest): added survey flow lib and added classification page
    
    * feat(research): added classification survey logic and components
    
    * feat(research): added contact us for cancellation in settings page
    
    * fix(research): sorted articles in home page
    
    * fix(research): added blog to the list of categories to block
    
    * fix(research):
    
    * feat(research): added blog and analysis to be free
    
    * fix(research): added the or clause for blog and analysis
    
    * fix()
    
    * fix(research): fixed sign in and paid problem
    
    * feat(research): added subtitle for article title
    
    * fix(research): added subtitles

--- apps/research/src/components/articles/ArticleTitle.tsx
+++ apps/research/src/components/articles/ArticleTitle.tsx
@@ -8,12 +8,14 @@ interface ArticleTitleProps {
   product: ProductTitleProps;
   published: string;
   title: string;
+  subtitle?: string;
 }
 
 const ArticleTitle: React.FC<ArticleTitleProps> = ({
   product,
   published,
   title,
+  subtitle,
 }) => {
   return (
     <div className="flex flex-col g-2">
@@ -27,6 +29,11 @@ const ArticleTitle: React.FC<ArticleTitleProps> = ({
       <p className="md:text-heading7 text-heading8 text-label-light-primary">
         {title}
       </p>
+      {subtitle && (
+        <p className="md:text-body2 text-small  md:text-label-light-primary text-label-light-secondary">
+          {subtitle}
+        </p>
+      )}
     </div>
   );
 };
