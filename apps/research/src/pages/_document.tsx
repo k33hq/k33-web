4d49189776e6f0c930a42ac87b81e007f9692037
Joel DSouza <joeldsouzax@gmail.com>
2023-03-28 12:23:11 +0200

    fix(k33): google analytics invest image about page and stuff

--- apps/research/src/pages/_document.tsx
+++ apps/research/src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   DocumentContext,
 } from 'next/document';
 import { poppins } from './_app';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -23,6 +24,25 @@ class MyDocument extends Document {
             sizes="64x64"
             href="/research/favicon-64x64.png"
           />
+          <Script
+            strategy="afterInteractive"
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          window.dataLayer.push(arguments);
+        }
+  
+        gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}');
+        gtag('consent', 'default', {
+          ad_storage: 'denied',
+          analytics_storage: 'denied',
+        });
+        `}
+          </Script>
         </Head>
         <body className={`${poppins.className} flex min-h-screen flex-col`}>
           <Main />
