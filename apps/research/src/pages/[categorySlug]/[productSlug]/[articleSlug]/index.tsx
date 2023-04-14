65006294510c0e525d2ac1263bc39d7cf1932314
Joel DSouza <joeldsouzax@gmail.com>
2023-04-14 16:19:24 +0200

    fix(research): added bottom padding to the article page

--- apps/research/src/pages/[categorySlug]/[productSlug]/[articleSlug]/index.tsx
+++ apps/research/src/pages/[categorySlug]/[productSlug]/[articleSlug]/index.tsx
@@ -156,7 +156,7 @@ const Article: NextPageWithLayout<ArticleProps> = ({
         </div>
 
         <article
-          className={`flex flex-col justify-center md:gap-8 gap-4 md:w-2/3 w-full px-6 md:px-0`}
+          className={`flex flex-col justify-center md:gap-8 gap-4 md:w-2/3 w-full px-6 md:px-0 pb-[120px]`}
         >
           <div className="flex flex-col gap-2">
             <div className="flex flex-col md:gap-2 gap-1">
