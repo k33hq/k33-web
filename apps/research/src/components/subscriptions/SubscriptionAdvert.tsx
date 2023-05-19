ee4a1e9c4dc9b3a8b96fc6062821f12d92b7869a
Joel DSouza <joeldsouzax@gmail.com>
2023-05-19 16:08:27 +0200

    fix(research): updated CTA changes

--- apps/research/src/components/subscriptions/SubscriptionAdvert.tsx
+++ apps/research/src/components/subscriptions/SubscriptionAdvert.tsx
@@ -84,7 +84,7 @@ const SubscriptionAdvert: React.FC<SubscriptionAdvertProps> = ({
         subtitle="Subscribe to K33 Research Pro to keep reading and get 30 days of full access to all our research."
       >
         <div className="items-center justify-center text-center flex flex-col lg:gap-4">
-          <Link href={process.env.NEXT_PUBLIC_PLATFORM_URL + 'auth'}>
+          <Link href={process.env.NEXT_PUBLIC_PLATFORM_URL + '/auth'}>
             <BasicButton variant="secondary">Sign Up & Start Trial</BasicButton>
           </Link>
 
@@ -93,7 +93,7 @@ const SubscriptionAdvert: React.FC<SubscriptionAdvertProps> = ({
               Already a paid subscriber?
             </p>
             <Link
-              href={process.env.NEXT_PUBLIC_PLATFORM_URL + 'auth'}
+              href={process.env.NEXT_PUBLIC_PLATFORM_URL + '/auth'}
               className="md:text-body2 text-small text-label-dark-primary underline"
             >
               Sign In
