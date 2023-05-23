a43b1c247f4e332bae15a233741dd65785bed3ec
Vihang Patil <vihang.patil@gmail.com>
2023-05-23 11:22:57 +0200

    chore(plaform): replacing platform with webflow landing pages

--- apps/platform/src/types/domain.ts
+++
@@ -1,4 +0,0 @@
-export interface EmailAddressContactList {
-  emails: Array<string>;
-  listIds: Array<string>;
-}
--- apps/platform/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-const sharedConfig = require('tailwind-config/tailwind.config.js');
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  ...sharedConfig,
-  plugins: [require('@tailwindcss/typography')],
-};
