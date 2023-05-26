f2891e79338751a0eddf975cb36e75fe0b427829
Vihang Patil <vihang.patil@gmail.com>
2023-05-26 10:10:08 +0200

    feat(ci): deploy platform pages

---
+++ infra/gcp/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#
+#  Script to deploy webflow landing pages to Firestore.
+#
+
+set -e
+
+if [ -z "${BASH_VERSINFO}" ] || [ -z "${BASH_VERSINFO[0]}" ] || [ ${BASH_VERSINFO[0]} -lt 4 ]; then
+  echo "This script requires Bash version >= 4"
+  exit 1
+fi
+
+# remove old files
+rm -rf apps/platform/out/*
+
+# unzip
+unzip k33-web.webflow.zip -d apps/platform/out/
+
+# add robots.txt and sitemap.xml 
+cp apps/platform/public/robots.txt apps/platform/out/
+cp apps/platform/public/sitemap.xml apps/platform/out/
+
+# select k33-prod project
+firebase use k33-prod
+
+# deploy
+firebase deploy --only hosting:k33-platform
