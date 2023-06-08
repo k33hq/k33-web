7f9d643414d124594fbfd5de1d3e6e118d93748e
Joel DSouza <joeldsouzax@gmail.com>
2023-06-08 16:15:27 +0200

    feat(research): added header and styling

--- apps/research/src/components/platform/ResearchHeader.tsx
+++ apps/research/src/components/platform/ResearchHeader.tsx
@@ -1,21 +1,73 @@
-import { Affix, Layout, theme } from 'antd';
+import {
+  Affix,
+  Layout,
+  Menu,
+  Grid,
+  Button,
+  Row,
+  Col,
+  Typography,
+  theme,
+  ConfigProvider,
+} from 'antd';
 import styles from './styles.module.scss';
+import { appStructure } from '@/config';
+import { MenuOutlined } from '@ant-design/icons';
 
 const { Header } = Layout;
+const { useBreakpoint } = Grid;
 
 interface ResearchHeaderProps {
   toggleSider: () => void;
 }
 
 const ResearchHeader: React.FC<ResearchHeaderProps> = ({ toggleSider }) => {
+  const { md } = useBreakpoint();
+
   return (
     <Affix>
       <Header className={styles.header}>
-        <div id="header-content">
-          <div>Logo</div>
-          <div>Menu</div>
-          <div>Sign up and sider</div>
-        </div>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: '#999999',
+              colorBgBase: '#141414',
+              colorText: '#FFFFFF',
+            },
+          }}
+        >
+          <div id="header-content">
+            <Row gutter={32}>
+              <Col>
+                <Typography.Text>Logo</Typography.Text>
+              </Col>
+              <Col flex={1}>
+                {md && (
+                  <Menu
+                    mode="horizontal"
+                    disabledOverflow
+                    items={appStructure.navigation.map(({ key, label }) => ({
+                      key,
+                      label,
+                    }))}
+                  />
+                )}
+              </Col>
+              <Col>
+                <Button>Sign In</Button>
+              </Col>
+              <Col>
+                {!md && (
+                  <Button
+                    onClick={toggleSider}
+                    type="text"
+                    icon={<MenuOutlined />}
+                  />
+                )}
+              </Col>
+            </Row>
+          </div>
+        </ConfigProvider>
       </Header>
     </Affix>
   );
