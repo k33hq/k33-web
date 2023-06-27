b4814d90b812739ed008a5056481c79570b8a7b6
Joel DSouza <joeldsouzax@gmail.com>
2023-06-27 17:09:42 +0200

    feat(research): added next-sitemap and configuration

---
+++ apps/research-archive/next-sitemap.config.js
@@ -0,0 +1,10 @@
+// Default code you can customize according to your requirements.
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://k33.com/research/archive',
+  generateRobotsTxt: false,
+  exclude: ['/articles', '/settings'],
+  autoLastmod: false,
+  generateIndexSitemap: false,
+};
