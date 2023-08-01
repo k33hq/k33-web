23f50ab03ccce11ced4596000de6fb3caa6e1d27
Joel DSouza <joeldsouzax@gmail.com>
2023-08-01 12:33:28 +0300

    refactor(research): removed hits component from search hits

--- apps/research/src/components/search/SearchHits.tsx
+++ apps/research/src/components/search/SearchHits.tsx
@@ -25,33 +25,6 @@ interface ResultBoxProps {
   };
 }
 
-// TODO: hooks
-
-const Hit: React.FC<ResultBoxProps> = ({ hit }) => {
-  const { status } = useInstantSearch();
-
-  const imageUrl = hit.image ? hit.image.url : '';
-  const imageDescription = hit.image ? hit.image.title : '';
-
-  return (
-    <>
-      {['loading', 'stalled', 'error'].includes(status) ? null : (
-        <ArticleCard
-          article={{
-            subtitle: hit.subtitle,
-            title: hit.title,
-
-            thumbnail: { url: imageUrl, description: imageDescription },
-            tagsCollection: { items: hit.tags.map((tag) => ({ title: tag })) },
-          }}
-          publishedDate={hit.publishedDate}
-          articleSlug={hit.slug}
-        />
-      )}
-    </>
-  );
-};
-
 const SearchHits: React.FC = () => {
   const { results } = useHits<{
     authors: ReadonlyArray<string>;
