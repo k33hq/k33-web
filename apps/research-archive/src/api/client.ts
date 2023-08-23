045bdc5ea860e82b7343c3ef9291f63a9d9e1830
Vihang Patil <vihang.patil@gmail.com>
2023-08-23 11:23:58 +0200

    chore(env): removed contetnful graphql endpoint env var

--- apps/research-archive/src/api/client.ts
+++ apps/research-archive/src/api/client.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
 export const contentful = new GraphQLClient(
-  `${process.env.NEXT_PUBLIC_CONTENTFUL_GRAPHQL_ENDPOINT}/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_ARCHIVE_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}`,
+  `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_ARCHIVE_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}`,
   {
     headers: {
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_CONTENTFUL_ARCHIVE_ACCESS_TOKEN}`,
