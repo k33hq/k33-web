8e776b00a4227d8b0261db7c97c987b9a113f3a2
Joel DSouza <joeldsouzax@gmail.com>
2023-12-15 10:52:48 +0100

    feat(invest redirection): added redirection from login page to invest

--- apps/invest/src/layouts/PrivateLayout.tsx
+++ apps/invest/src/layouts/PrivateLayout.tsx
@@ -15,7 +15,7 @@ const PrivateLayout: React.FC<PrivateLayoutProps> = ({ children }) => {
   React.useEffect(() => {
     // TODO: redirect to login
     if (state === 'SIGNED_OUT') {
-      window.location.href = `https://${process.env.NEXT_PUBLIC_WEB_DOMAIN}/services/auth`;
+      window.location.href = `https://${process.env.NEXT_PUBLIC_WEB_DOMAIN}/services/auth?redirect=https://${process.env.NEXT_PUBLIC_WEB_DOMAIN}/apps/invest/classification`;
     }
   }, [state, router]);
 
