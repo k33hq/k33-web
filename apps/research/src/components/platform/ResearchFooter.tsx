1f841776ece87726095d29f7de02bb868c3b3b45
Joel DSouza <joeldsouzax@gmail.com>
2024-01-09 16:54:40 +0100

    feat(twitter tracking): added twitter tracking script

--- apps/research/src/components/platform/ResearchFooter.tsx
+++ apps/research/src/components/platform/ResearchFooter.tsx
@@ -49,6 +49,19 @@ const ResearchFooter: React.FC = () => {
           src="https://px.ads.linkedin.com/collect/?pid=2684138&fmt=gif"
         />
       </noscript>
+
+      <Script
+        type="text/javascript"
+        strategy="afterInteractive"
+        id="twitter_k33_script"
+        dangerouslySetInnerHTML={{
+          __html: `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+  },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+  a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+  twq('config','o56y4');`,
+        }}
+      />
+
       <Divider />
       <div
         style={{
