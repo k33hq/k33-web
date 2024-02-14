bbd0c14fa7851e7269484d4b6f77c84f5e78b145
Joel DSouza <joeldsouzax@gmail.com>
2024-02-14 17:20:15 +0100

    feat(hojar): added hotjar script

--- apps/research/src/pages/_app.tsx
+++ apps/research/src/pages/_app.tsx
@@ -62,6 +62,24 @@ const ResearchApp = ({ Component, ...rest }: ResearchAppProps) => {
           `,
         }}
       />
+      <Script
+        id="hotjar-script"
+        defer
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3855489,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
+
       <div style={{ visibility: !mounted ? 'hidden' : 'visible' }}>
         <MainLayout>{getLayout(<Component {...props.pageProps} />)}</MainLayout>
       </div>
