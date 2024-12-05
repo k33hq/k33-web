269452aa64216ace5a410196ce1ac644eef4b941
Vihang Patil <vihang.patil@gmail.com>
2024-12-05 15:24:06 +0530

    fix(research-archive): fixed issue of incorrect app state

--- apps/research-archive/src/pages/_app.tsx
+++ apps/research-archive/src/pages/_app.tsx
@@ -1,18 +1,11 @@
 import { AppProps } from 'next/app';
-import { Manrope } from '@next/font/google';
 import * as React from 'react';
 import { NextPageWithLayout } from 'platform-js';
 import withTheme from '../theme';
 import { MainLayout } from '@/components';
 import '../../public/antd.min.css';
 import '../styles/globals.scss';
-
-export const manrope = Manrope({
-  weight: ['300', '400', '500', '600'],
-  display: 'swap',
-  subsets: ['latin'],
-  variable: '--font-manrope',
-});
+import Script from 'next/script';
 
 interface ResearchAppProps extends AppProps {
   Component: NextPageWithLayout;
@@ -36,10 +29,33 @@ const ResearchApp = ({ Component, ...rest }: ResearchAppProps) => {
         id="holderStyle"
         dangerouslySetInnerHTML={{
           __html: `
-                *, *::before, *::after {
-                    transition: none!important;
-                }
-                `,
+                    *, *::before, *::after {
+                        transition: none!important;
+                    }
+                    `,
+        }}
+      />
+      <Script
+        rel="preconnect"
+        strategy="afterInteractive"
+        defer
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-script"
+        defer
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}');
+            gtag('consent', 'default', {
+              ad_storage: 'denied',
+              analytics_storage: 'denied',
+            });
+          `,
         }}
       />
       <div style={{ visibility: !mounted ? 'hidden' : 'visible' }}>
